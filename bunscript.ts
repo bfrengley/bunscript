1433daacd111c183748ee550b389a652b4526c2b
Ben Frengley <ben.frengley@gmail.com>
2025-09-25 22:59:36 +1200

    Replace manual linking with bun link

--- bunscript.ts
+++ bunscript.ts
@@ -2,14 +2,7 @@
 
 import { $ } from "bun";
 import util from "util";
-import {
-  exists,
-  readlink as fsreadlink,
-  realpath,
-  readdir,
-  symlink,
-  unlink,
-} from "node:fs/promises";
+import { readdir } from "node:fs/promises";
 import path from "node:path";
 import { exit } from "node:process";
 
@@ -20,18 +13,10 @@ Usage: bunscript new <script-name> [--edit] [--no-link] - Create a new bun scrip
        bunscript link                                   - Link existing scripts into $PATH
        bunscript unlink [script-name]                   - Unlink existing scripts from $PATH (only the named script if provided)
        bunscript --help|-h                              - Print this message
-       
-The location into which scripts will be linked will be chosen from the following list, in order of preference:
-  - $BUNSCRIPT_BIN_DIR
-  - $HOME/bin
-  - $HOME/.local/bin
-If none of those directories exist, linking will fail.
-
-The shebang of new scripts can be controlled using the $BUNSCRIPT_SHEBANG variable. If not set, it defaults to \`#!/usr/bin/env bun\`.
 `);
 }
 
-const SCRIPT_DIR = path.join(__dirname, "scripts");
+const SCRIPT_DIR = "./scripts";
 
 if (Bun.argv.length <= 2) {
   usage();
@@ -69,6 +54,7 @@ try {
   }
 } catch (e) {
   console.error("error:", e instanceof Error ? e.message : e);
+  console.error(e);
 }
 
 async function createscript(args: string[]): Promise<void> {
@@ -88,7 +74,7 @@ async function createscript(args: string[]): Promise<void> {
   }
   const [scriptName] = positionals;
 
-  const scriptTemplate = `${Bun.env.BUNSCRIPT_SHEBANG || "#!/usr/bin/env bun"}
+  const scriptTemplate = `#!/usr/bin/env bun
 
 import { $ } from "bun";
 
@@ -100,7 +86,14 @@ await $\`echo Hello, world!\`;
     exit(2);
   }
 
-  const scriptPath = path.join(SCRIPT_DIR, scriptName + ".ts");
+  if (scriptName === "bunscript") {
+    // maybe there's a better way to handle this
+    console.error("the name 'bunscript' will collide with this file!");
+    exit(1);
+  }
+
+  const scriptRelPath = path.join(SCRIPT_DIR, scriptName + ".ts");
+  const scriptPath = path.normalize(path.join(__dirname, scriptRelPath));
   const scriptFile = Bun.file(scriptPath);
   if (await scriptFile.exists()) {
     console.error(`script ${scriptName} already exists`);
@@ -115,8 +108,7 @@ await $\`echo Hello, world!\`;
   }
 
   if (values["no-link"] !== true) {
-    const linkDir = (await findLinkDirs())[0]!;
-    await linkScript(linkDir, scriptPath);
+    await linkScripts([scriptRelPath]);
   }
 }
 
@@ -126,7 +118,7 @@ async function removescript(script: string): Promise<void> {
     exit(2);
   }
 
-  const scriptPath = path.join(SCRIPT_DIR, script + ".ts");
+  const scriptPath = absoluteScriptPath(script);
   const scriptFile = Bun.file(scriptPath);
   if (!(await scriptFile.exists())) {
     console.error(`couldn't find script ${script}`);
@@ -136,157 +128,98 @@ async function removescript(script: string): Promise<void> {
   await unlinkscripts(script);
 
   await scriptFile.delete();
-  console.log("removed script", script);
+  console.log("removed script file", scriptPath);
 }
 
 async function linkscripts(): Promise<void> {
-  let exitCode = 0;
-
-  const linkdir = (await findLinkDirs())[0]!; // ! is safe since at least one is always returned
-
-  for (const script of await readdir(SCRIPT_DIR)) {
+  const scripts: string[] = [];
+  for (const script of await readdir(absoluteScriptPath())) {
     if (!script || !script.endsWith(".ts")) {
       continue;
     }
 
-    const source = await realpath(path.join(SCRIPT_DIR, script));
-    if (!(await linkScript(linkdir, source))) {
-      exitCode = 1;
+    const source = absoluteScriptPath(script);
+    scripts.push(source);
+  }
+
+  await linkScripts(scripts);
+}
+
+async function linkScripts(scriptPaths: string[]): Promise<void> {
+  const self = "bunscript";
+  const pkgFile = Bun.file(path.join(__dirname, "package.json"));
+  const pkgJson: PackageJson = await pkgFile.json();
+
+  for (const scriptPath of scriptPaths) {
+    const scriptName = path.basename(scriptPath, ".ts");
+    if (scriptName === self) {
+      console.warn("ignoring colliding script", self);
+      continue;
     }
+    pkgJson.bin[scriptName] = scriptPath;
+    console.log(
+      `linked script ${scriptName} at ${absoluteScriptPath(scriptName)}`
+    );
   }
 
-  exit(exitCode);
-}
-
-async function linkScript(
-  linkDir: string,
-  scriptPath: string
-): Promise<boolean> {
-  const scriptName = path.basename(scriptPath, ".ts");
-  const target = path.join(linkDir, scriptName);
-  const state = await linkState(scriptPath, target);
-
-  if (state === LinkState.other) {
-    console.error(`failed to link ${scriptName}: ${target} already exists`);
-    return false;
-  } else if (state === LinkState.valid) {
-    console.log(`skipped ${scriptName}: link already exists`);
-  } else {
-    await symlink(scriptPath, target, "file");
-    await $`chmod +x ${target}`;
-    console.log(`linked ${scriptName} as ${target}`);
+  // always ensure that this file remains linked
+  pkgJson.bin[self] = `./${self}.ts`;
+  Bun.write(pkgFile, JSON.stringify(pkgJson, null, 2));
+  const res = await $`bun link -f`.cwd(__dirname).nothrow().quiet();
+  if (res.exitCode !== 0) {
+    throw new Error(`failed to link scripts: ${res.stderr.toString("utf-8")}`);
   }
-
-  return true;
 }
+
+type PackageJson = {
+  bin: Record<string, string>;
+};
 
 async function unlinkscripts(limitToScript: string): Promise<void> {
-  const linkdirs = await findLinkDirs();
+  const self = "bunscript";
+  const pkgFile = Bun.file(path.join(__dirname, "package.json"));
+  const pkgJson: PackageJson = await pkgFile.json();
 
-  for (const dir of linkdirs) {
-    for (const file of await readdir(dir)) {
-      if (!file) {
-        continue;
-      }
-
-      const fullPath = path.join(dir, file);
-
-      if (await fileIsLink(fullPath)) {
-        const target = await fsreadlink(fullPath);
-        if (path.dirname(target) !== SCRIPT_DIR) {
-          continue;
-        }
-
-        const scriptName = path.basename(target, ".ts");
-        if (limitToScript && scriptName !== limitToScript) {
-          continue;
-        }
-
-        await unlink(fullPath);
-        console.log("unlinked link to script", scriptName);
-      }
+  for (const k of Object.keys(pkgJson.bin)) {
+    if (k === self) {
+      // don't accidentally unlink the main script
+      continue;
     }
-  }
-}
 
-const enum LinkState {
-  /** No link or file exists */
-  none,
-  /** A valid link to a known script exists */
-  valid,
-  /** A file exists that is not a valid link to a known script */
-  other,
-}
-
-async function linkState(script: string, target: string): Promise<LinkState> {
-  if (await fileIsLink(target)) {
-    if ((await fsreadlink(target)) === script) {
-      return LinkState.valid;
+    if (limitToScript && k !== limitToScript) {
+      continue;
     }
-    return LinkState.other;
+
+    delete pkgJson.bin[k];
+    console.log(`unlinked script ${k}`);
   }
 
-  if (await Bun.file(target).exists()) {
-    return LinkState.other;
+  // unlink before we remove the scripts to make sure they get removed
+  const unlinkRes = await $`bun unlink`.cwd(__dirname).nothrow().quiet();
+  if (unlinkRes.exitCode !== 0) {
+    throw new Error(
+      `failed to unlink scripts: unlink ${unlinkRes.stderr.toString("utf-8")}`
+    );
   }
 
-  return LinkState.none;
+  // save and relink
+  Bun.write(pkgFile, JSON.stringify(pkgJson, null, 2));
+  const res = await $`bun link`.cwd(__dirname).nothrow().quiet();
+  if (res.exitCode !== 0) {
+    throw new Error(
+      `failed to unlink scripts: relink ${res.stderr.toString("utf-8")}`
+    );
+  }
 }
 
-/**
- * This is a workaround for `Bun.file(...).exists()` failing on symlinks on Windows. I don't think this will
- * work in shells other than bash but that's fine.
- */
-async function fileIsLink(file: string): Promise<boolean> {
-  if (process.platform === "win32") {
-    return (await $`test -h ${file}`.nothrow().quiet()).exitCode === 0;
-  }
-  return (await Bun.file(file).stat()).isSymbolicLink();
-}
-
-async function readlink(file: string): Promise<string> {
-  if (process.platform === "win32") {
-    throw new Error("unimplemented");
-  }
-  return fsreadlink(file);
-}
-
-async function findLinkDirs(): Promise<string[]> {
-  const dirs: string[] = [];
-  if (Bun.env.BUNSCRIPT_BIN_DIR) {
-    const dir = Bun.file(Bun.env.BUNSCRIPT_BIN_DIR);
-    if (!path.isAbsolute(Bun.env.BUNSCRIPT_BIN_DIR)) {
-      console.warn("$BUNSCRIPT_BIN_DIR is set to a relative path; ignoring");
-    } else if (
-      (await exists(Bun.env.BUNSCRIPT_BIN_DIR)) &&
-      (await dir.stat()).isDirectory()
-    ) {
-      dirs.push(Bun.env.BUNSCRIPT_BIN_DIR);
-    } else {
-      console.warn(
-        `$BUNSCRIPT_BIN_DIR is set to ${Bun.env.BUNSCRIPT_BIN_DIR} but no such valid directory exists; ignoring`
-      );
-    }
+function absoluteScriptPath(script?: string): string {
+  if (!script) {
+    // just the script dir
+    return path.normalize(path.join(__dirname, SCRIPT_DIR));
   }
 
-  const home = Bun.env.HOME;
-  if (home) {
-    for (const d in [
-      path.join(home, "bin"),
-      path.join(home, ".local", "bin"),
-    ]) {
-      const dir = Bun.file(d);
-      // use fs.exists not Bun.file().exists due to the latter returning false for directories
-      if ((await exists(d)) && (await dir.stat()).isDirectory()) {
-        dirs.push(d);
-      }
-    }
-  } else {
-    console.warn("$HOME isn't set"); // should never happen
+  if (!script.endsWith(".ts")) {
+    script += ".ts";
   }
-
-  if (dirs.length === 0) {
-    throw new Error("could not find a directory suitable for script links");
-  }
-  return dirs;
+  return path.normalize(path.join(__dirname, SCRIPT_DIR, script));
 }
